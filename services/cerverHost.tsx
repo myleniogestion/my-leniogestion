ad6fe670e6c1d8b876514a02d3fa6019fe321cea
myleniogestion <myleniogestion@gmail.com>
2025-03-16 01:30:36 -0400

    Listo para el despliegue

--- services/cerverHost.tsx
+++ services/cerverHost.tsx
@@ -1,4 +1,4 @@
 
-export const cerverHost = "https://my-leniogestionbackend.onrender.com";
-export const cerverHostImages = "https://my-leniogestionbackend.onrender.com";
+export const cerverHost = "https://my-leniogestionbackend-w5gc.onrender.com";
+export const cerverHostImages = "https://my-leniogestionbackend-w5gc.onrender.com";
 // 147.93.128.46
